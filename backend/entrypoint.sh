7882f002579d473060feb1cc543c8954d38d1b24
juani <jivila@outlook.com>
2023-12-10 18:17:07 -0300

    entrypoint.sh now creates two different superusers

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 echo "Apply database migrations"
+python manage.py makemigrations
 python manage.py migrate
 chmod +w /usr/src/app/db.sqlite3
 
@@ -9,5 +10,11 @@ echo "Create Django superuser"
 DJANGO_SUPERUSER_PASSWORD="admin" python manage.py createsuperuser --noinput \
     --username=admin \
     --email=admin@example.com
+
+
+echo "Create Django superuser 2"
+DJANGO_SUPERUSER_PASSWORD="admin" python manage.py createsuperuser --noinput \
+    --username=admin2 \
+    --email=admin2@example.com
 
 exec "$@"
