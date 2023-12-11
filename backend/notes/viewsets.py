6af02f5c05c734d50e238d91d3a74be6aac40c97
juani <jivila@outlook.com>
2023-12-11 17:22:09 -0300

    Feature: filtering and searching implemented

--- backend/notes/viewsets.py
+++ backend/notes/viewsets.py
@@ -1,4 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Todo
@@ -7,8 +9,12 @@ from .serializers import TaskSerializer
 
 class TaskListCreateApiView(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
-    # queryset = Todo.objects.all()
     serializer_class = TaskSerializer
+
+    # Add filtering and search by title and description
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ["completed", "due_date"]
+    search_fields = ["title", "description"]
 
     def get_queryset(self):
         user = self.request.user
